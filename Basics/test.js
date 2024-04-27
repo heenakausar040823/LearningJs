c0dfe410cb11ed7a1da8eeefd332d3859bfe4b68
Heena Kausar <141433043+heenakausar040823@users.noreply.github.com>
2024-04-27 14:28:11 +0000

    datatypes in js

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,19 @@
-console.log("hello")
+//DATA TYPES IN JS
+
+let firstName = "john" //string => dbl quote or single quote
+
+let num = 33 // number => range is 2 ^ 53
+
+//bigint
+
+let isLoggedIn = true // boolean => true/ false
+
+let x; // undefined (type is undefined)
+console.log(typeof x) // undefined
+
+let temprature = null; //null (type is object)
+console.log(typeof temprature) // object
+
+//symbol => shows uniqueness
+
+//object
